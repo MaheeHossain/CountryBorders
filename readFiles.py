874ae059e77b278be3c7d9177d084149afd86c7d
Mahee Hossain <mahee.hossain@student.unimelb.edu.au>
2021-08-19 16:07:51 +1000

    Added list of alternative country names

--- readFiles.py
+++ readFiles.py
@@ -2,11 +2,12 @@
 # Functions to help deal with files
 
 INPUT_FILE = './Test_cases/Input.txt'
+DEMO_FILE = './Test_cases/Demo.txt'
 ALT_NAMES = './Test_cases/alternativeNames.txt'
 
 def readInput():
     # Reads the input file and returns an array with all countries
     lines=[]
-    with open(ALT_NAMES) as f:
+    with open(INPUT_FILE) as f:
         lines = f.readlines()
     return lines
